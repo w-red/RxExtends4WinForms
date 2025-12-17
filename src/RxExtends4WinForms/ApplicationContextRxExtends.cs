56f222733df69a4a9de758eff450aee47d8ef5c1
terashi <yterashi@gmail.com>
2025-12-17 10:28:11 +0900

    initial commit.

---
+++ src/RxExtends4WinForms/ApplicationContextRxExtends.cs
@@ -0,0 +1,17 @@
+﻿using System.Reactive.Linq;
+
+namespace RxExtends4WinForms;
+
+/// <summary><see cref="System.Reactive"/>'s extensions for <see cref="ApplicationContext"/></summary>
+public static class ApplicationContextRxExtends
+{
+    extension(ApplicationContext ac)
+    {
+        /// <summary>Converts the <see cref="ApplicationContext.ThreadExit"/> event to an observable sequence</summary>
+        public IObservable<EventArgs> ThreadExitAsObservable() =>
+            Observable.FromEvent<EventHandler, EventArgs>(
+                h => (s, e) => h(e),
+                h => ac.ThreadExit += h,
+                h => ac.ThreadExit -= h);
+    }
+}
---
+++ src/RxExtends4WinForms/ApplicationRxExtends.cs
@@ -0,0 +1,52 @@
+﻿using System.Reactive.Linq;
+
+namespace RxExtends4WinForms;
+
+/// <summary><see cref="System.Reactive"/>'s extensions for <see cref="Application"/></summary>
+public static class ApplicationRxExtends
+{
+    extension(Application app)
+    {
+        /// <summary>Converts the <see cref="Application.ApplicationExit"/> event as <see cref="Observable"/></summary>
+        public static IObservable<EventArgs> ApplicationExitAsObservable() =>
+            Observable.FromEvent<EventHandler, EventArgs>(
+                h => (s, e) => h(e),
+                h => Application.ApplicationExit += h,
+                h => Application.ApplicationExit -= h);
+
+        /// <summary>Converts the <see cref="Application.EnterThreadModal"/> event as <see cref="Observable"/></summary>
+        public static IObservable<EventArgs> EnterThreadModalAsObservable() =>
+            Observable.FromEvent<EventHandler, EventArgs>(
+                h => (s, e) => h(e),
+                h => Application.EnterThreadModal += h,
+                h => Application.EnterThreadModal -= h);
+
+        /// <summary>Converts the <see cref="Application.Idle"/> event as <see cref="Observable"/></summary>
+        public static IObservable<EventArgs> IdleAsObservable() =>
+            Observable.FromEvent<EventHandler, EventArgs>(
+                h => (s, e) => h(e),
+                h => Application.Idle += h,
+                h => Application.Idle -= h);
+
+        /// <summary>Converts the <see cref="Application.LeaveThreadModal"/> event as <see cref="Observable"/></summary>
+        public static IObservable<EventArgs> LeaveThreadModalAsObservable() =>
+            Observable.FromEvent<EventHandler, EventArgs>(
+                h => (s, e) => h(e),
+                h => Application.LeaveThreadModal += h,
+                h => Application.LeaveThreadModal -= h);
+
+        /// <summary>Converts the <see cref="Application.ThreadException"/> event as <see cref="Observable"/></summary>
+        public static IObservable<ThreadExceptionEventArgs> ThreadExceptionAsObservable() =>
+            Observable.FromEvent<ThreadExceptionEventHandler, ThreadExceptionEventArgs>(
+                h => (s, e) => h(e),
+                h => Application.ThreadException += h,
+                h => Application.ThreadException -= h);
+
+        /// <summary>Converts the <see cref="Application.ThreadExit"/> event as <see cref="Observable"/></summary>
+        public static IObservable<EventArgs> ThreadExitAsObservable() =>
+            Observable.FromEvent<EventHandler, EventArgs>(
+                h => (s, e) => h(e),
+                h => Application.ThreadExit += h,
+                h => Application.ThreadExit -= h);
+    }
+}
